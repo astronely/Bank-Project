65644f7c0ae35286d07f3afd0d4a9e0db8bdf93b
astronely <nikborowik200394@gmail.com>
2023-06-10 18:22:27 +0500

    add config file and some mock files

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,23 +2,23 @@ package db
 
 import (
 	"database/sql"
+	"github.com/astronely/simplebank/util"
 	_ "github.com/lib/pq"
 	"log"
 	"os"
 	"testing"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
-
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..") // ".." means go to the parents folder
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
