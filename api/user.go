4f124e4b7100ea287dcbac61ea85374622b96454
astronely <nikborowik200394@gmail.com>
2023-06-17 16:39:48 +0500

    add hash passwords

---
+++ api/user.go
@@ -0,0 +1,68 @@
+package api
+
+import (
+	db "github.com/astronely/simplebank/db/sqlc"
+	"github.com/astronely/simplebank/util"
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	"net/http"
+	"time"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+type createUserResponse struct {
+	Username         string    `json:"username"`
+	FullName         string    `json:"fullName"`
+	Email            string    `json:"email"`
+	PasswordChangeAt time.Time `json:"passwordChangeAt"`
+	CreatedAt        time.Time `json:"createdAt"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashedPassword,
+		FullName:       req.FullName,
+		Email:          req.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	rsp := createUserResponse{
+		Username:         user.Username,
+		FullName:         user.FullName,
+		Email:            user.Email,
+		PasswordChangeAt: user.PasswordChangeAt,
+		CreatedAt:        user.CreatedAt,
+	}
+	ctx.JSON(http.StatusOK, rsp)
+}
